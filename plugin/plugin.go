e51394c1b59ac52bb9bb5a83f3557758e7ce35d0
Jeff <jeffy.mathew100@gmail.com>
2021-11-22 12:48:47 +0530

    add init function

--- plugin/plugin.go
+++ plugin/plugin.go
@@ -2,7 +2,9 @@ package main
 
 import "fmt"
 
-
+func init() {
+	fmt.Println("initialized plugin")
+}
 func PluginFunc(pluginInputNumber int) { fmt.Printf("My custom code prints PluginInputNumber: %d\n", pluginInputNumber) }
 
 func Greet(pluginInputNumber int) {fmt.Printf("hello!! greetings!! x %d", pluginInputNumber)}
