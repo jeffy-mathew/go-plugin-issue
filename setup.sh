33371abef90e97ffe270e763005abe2461878b4e
Jeff <jeffy.mathew100@gmail.com>
2021-11-24 11:12:08 +0530

    remove setup.sh

--- setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd plugin
-go build -buildmode=plugin -o plugin1.so plugin.go
-go build -buildmode=plugin -o plugin2.so plugin.go
-cp {plugin1.so,plugin2.so} ../driver
-cd ../driver
-go build main.go
